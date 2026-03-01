5aab4a26f037259a557b97f0d6fd173b93c5810b
ephraim <ephraims.818@gmail.com>
2026-03-01 15:22:40 +0600

    JS--COMPLETE-GUIDE -- OBJECTS -- 01. Objects

---
+++ 01. Objects/Objects/assets/scripts/objects.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const person = {
+    name: "Ephraim",
+    age: 37,
+    hobbies: ["Sports", "Coding"],
+
+    greet: function () {
+        console.log(`Hi there!`);
+    },
+};
+
+console.log(person);
+person.greet();
