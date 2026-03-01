a1761d60920b3712aa3b14a23d52ecab8190dfb8
ephraim <ephraims.818@gmail.com>
2026-03-01 15:38:13 +0600

    JS--COMPLETE-GUIDE -- OBJECTS -- 02. Adding, Modifying and Deleting Properties

--- 01. Objects/Objects/assets/scripts/objects.js
+++ 01. Objects/Objects/assets/scripts/objects.js
@@ -1,14 +1,25 @@
 "use strict";
 
-const person = {
+let person = {
     name: "Ephraim",
-    age: 37,
+    age: 30,
     hobbies: ["Sports", "Coding"],
+
+    deleteThis: "yes",
 
     greet: function () {
         console.log(`Hi there!`);
     },
 };
-
 console.log(person);
 person.greet();
+
+// Adding property
+person.isAdmin = true;
+
+// Modifying property
+person.age = 38;
+
+// Deleting property
+delete person.deleteThis;
+console.log(person);
